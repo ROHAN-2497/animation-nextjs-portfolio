d06f6b1255cfba49f7f470907866839476b40e9f
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-20 02:23:52 +0600

    up

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import NavLinks from './NavLinks';
 const links = [
     { url: "/", title: "home" },
     { url: "/about", title: "About" },
@@ -12,23 +13,25 @@ const links = [
 const NavBar = () => {
     const [open, setOpen] = useState(false);
     return (
-        <div className=' h-full flex items-center justify-between px-4  md:px-12'>
-            <div className='hidden md:flex gap-4'>
+        <div className=' h-full flex items-center justify-between px-4  md:px-12 text-xl'>
+            {/* LINKS  */}
+            <div className='hidden md:flex gap-4 w-1/3'>
                 {
                     links.map(link => (
-                        <Link href={link.url} key={link.title}>{link.title}</Link>
+                       <NavLinks link={link} key={link.title} />
                     ))
                 }
             </div>
             {/* LOGO */}
-            <div className='md:hidden lg:flex gap-8'>
+            <div className='md:hidden lg:flex gap-8 w-1/3 justify-center items-center'>
                 <Link href="/"
                     className='bg-black rounded-md p-1 font-semibold text-sm flex items-center justify-center  '
                 >
                     <span className='text-white mr-1'>Rohan </span>
                     <span className='bg-white rounded-sm flex items-center justify-center  w-12 h-8'>.Dev</span>
                 </Link>
-                <div className='hidden md:flex gap-4 '>
+                <div className='hidden md:flex gap-4 w-1/3 '>
+                    {/* SOCIAL  */}
                     <Link href="/">
                         <Image src='/facebook.png' alt='' width={24} height={24}></Image>
                     </Link>
