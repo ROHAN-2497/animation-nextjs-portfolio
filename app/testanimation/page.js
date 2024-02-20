860b7cc1dfc638d45402d021774721197e877589
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-21 02:00:48 +0600

    up

--- app/testanimation/page.js
+++ app/testanimation/page.js
@@ -18,7 +18,7 @@ const TestPage = () => {
     return (
         <div className='h-full flex items-center justify-center'
         >
-            < motion.div className='w-96 h-96 bg-red-500 rounded' initial={{ x: -100 }} animate={{ x: 400, y: 300, opacity: 0.5 }}
+            < motion.div className='w-96 h-96 bg-red-500 rounded' initial={{ x: -100 }} variants={variants} animate="variants2"
                 transition={{ delay: 2, duration: 4 }}></motion.div>
         </div>
     );
