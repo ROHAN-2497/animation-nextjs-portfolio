aa6ead31f320bf3d2b137662040b07888773d30e
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-20 22:30:00 +0600

    up

--- app/page.js
+++ app/page.js
@@ -2,35 +2,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-<div><Image
-src='/rohan.jpg' alt="" fill className="object-contain"
- /></div>
-<div></div>
-   </div>
+    <div className="flex   ">
+      <div className="w-1/2"><Image
+        src='/rohan.jpg' alt="" width={1000} height={1000} className="object-contain rounded-s-full"
+      /></div>
+
+      <div className="">
+        {/* TITLE  */}
+        <h1 className="text-4xl font-bold md-6xl">Crafting  Digital Expreiences, Desining Tomorrow</h1>
+        {/* DESC  */}
+        <p className="md:text-xl">Welcome to my digital canvas, Where innovation and creativity converge. With a keen eye for aesthetics and mastery of code , my portfolio showcases a diverse collection of projects that reflect my commeitment to excellence</p>
+        {/* BUTTON  */}
+        <div className="flex gap-4 w-full">
+          <button className="rounded p-4 ring-1 ring-black bg-black text-white">View My Work</button>
+          <button className="rounded p-4 ring-1 ring-black ">Contact Me</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
 
 
-//  <div className=" h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-// {/* IMAGE CONTAINER */}
-// <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-//   <Image
-//     src="/hero.png" alt="" fill className="object-contain"
-//   />
-// </div>
-// {/* TEXT CONTAINER */}
-// <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 ">
-//   {/* TITLE */}
-//   <h1 className=" md:text-6xl text-4xl font-bold">Crafting  Digital Expreiences, Desining Tomorrow</h1>
-//   {/* DESC  */}
-//   <p className="md:text-xl">Welcome to my digital canvas, Where innovation and creativity converge. With a keen eye for aesthetics and mastery of code , my portfolio showcases a diverse collection of projects that reflect my commeitment to excellence</p>
-//   {/* BUTTON  */}
-//   <div className="flex gap-4 w-full">
-//     <button className="rounded p-4 ring-1 ring-black bg-black text-white">View My Work</button>
-//     <button className="rounded p-4 ring-1 ring-black ">Contact Me</button>
-//   </div>
-// </div>
+{/* <div className=" h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48"> */ }
+{/* IMAGE CONTAINER */ }
+<div className="h-1/2 lg:h-full lg:w-1/2 relative">
+  <Image
+    src="/hero.png" alt="" fill className="object-contain"
+  />
+</div>
+{/* TEXT CONTAINER */ }
+<div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 ">
+  {/* TITLE */}
+  <h1 className=" md:text-6xl text-4xl font-bold">Crafting  Digital Expreiences, Desining Tomorrow</h1>
+  {/* DESC  */}
+  <p className="md:text-xl">Welcome to my digital canvas, Where innovation and creativity converge. With a keen eye for aesthetics and mastery of code , my portfolio showcases a diverse collection of projects that reflect my commeitment to excellence</p>
 
-// </div> 
+</div>
+
