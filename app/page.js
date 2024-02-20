34d6bc418215102ea7e909fd0cfe7863d7cc9af5
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-20 09:23:20 +0600

    up

--- app/page.js
+++ app/page.js
@@ -2,8 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      Home
-    </main>
+    <div className="flex flex-col">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2">
+        <Image
+          src="/rohan.jpg" alt="fill" width={100} height={100}
+        />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2">
+        <h1>Crafting  Digital Expreiences , Desining Tomorrow</h1>
+        <p>Welcome to my digital canvas, Where innovation and creativity converge. With a keen eye for aesthetics and mastery of code , my portfolio showcases a diverse collection of projects that reflect my commeitment to excellence</p>
+      </div>
+    </div>
   );
 }
