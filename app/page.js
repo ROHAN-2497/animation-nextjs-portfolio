cdda106ce096afcf619344c6c52b1db8bf2e2b8f
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-20 09:55:03 +0600

    up

--- app/page.js
+++ app/page.js
@@ -2,25 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="  flex flex-col lg-flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className=" h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* IMAGE CONTAINER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
         <Image
-          src="/rohan.jpg" alt="" fill className="objectContain" 
+          src="/hero.png" alt="" fill className="object-contain"
         />
       </div>
       {/* TEXT CONTAINER */}
-      <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
+      <div className="h-1/2 lg:h-full lg:w-1/2  flex flex-col gap-8 items-center justify-center">
         {/* TITLE */}
         <h1 className="md:text-6xl text-4xl font-bold">Crafting  Digital Expreiences, Desining Tomorrow</h1>
         {/* DESC  */}
         <p className="md:text-xl">Welcome to my digital canvas, Where innovation and creativity converge. With a keen eye for aesthetics and mastery of code , my portfolio showcases a diverse collection of projects that reflect my commeitment to excellence</p>
         {/* BUTTON  */}
-        <div className="flex gap-4">
+        <div className="flex gap-4 w-full">
           <button className="rounded p-4 ring-1 ring-black bg-black text-white">View My Work</button>
           <button className="rounded p-4 ring-1 ring-black ">Contact Me</button>
         </div>
-
       </div>
     </div>
   );
