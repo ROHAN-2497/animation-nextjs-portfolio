cf60a0165d110c6c82a4276c3fff2730b9c352fd
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-20 22:16:10 +0600

    up

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}><div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
-        <div className="h-24"><NavBar/></div>
-        <div className="h-24">{children}</div>
+        <div className="h-24"><NavBar/>
+        
+        </div>
+        {children}
         </div></body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,28 +1,36 @@
-import PAge from "@/components/new";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      {/* IMAGE CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-        <Image
-          src="/hero.png" alt="" fill className="object-contain"
-        />
-      </div>
-      {/* TEXT CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
-        {/* TITLE */}
-        <h1 className="md:text-6xl text-4xl font-bold">Crafting  Digital Expreiences, Desining Tomorrow</h1>
-        {/* DESC  */}
-        <p className="md:text-xl">Welcome to my digital canvas, Where innovation and creativity converge. With a keen eye for aesthetics and mastery of code , my portfolio showcases a diverse collection of projects that reflect my commeitment to excellence</p>
-        {/* BUTTON  */}
-        <div className="flex gap-4 w-full">
-          <button className="rounded p-4 ring-1 ring-black bg-black text-white">View My Work</button>
-          <button className="rounded p-4 ring-1 ring-black ">Contact Me</button>
-        </div>
-      </div>
-
-    </div>
+   <div>
+<div><Image
+src='/rohan.jpg' alt="" fill className="object-contain"
+ /></div>
+<div></div>
+   </div>
   );
 }
+
+
+
+//  <div className=" h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+// {/* IMAGE CONTAINER */}
+// <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+//   <Image
+//     src="/hero.png" alt="" fill className="object-contain"
+//   />
+// </div>
+// {/* TEXT CONTAINER */}
+// <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 ">
+//   {/* TITLE */}
+//   <h1 className=" md:text-6xl text-4xl font-bold">Crafting  Digital Expreiences, Desining Tomorrow</h1>
+//   {/* DESC  */}
+//   <p className="md:text-xl">Welcome to my digital canvas, Where innovation and creativity converge. With a keen eye for aesthetics and mastery of code , my portfolio showcases a diverse collection of projects that reflect my commeitment to excellence</p>
+//   {/* BUTTON  */}
+//   <div className="flex gap-4 w-full">
+//     <button className="rounded p-4 ring-1 ring-black bg-black text-white">View My Work</button>
+//     <button className="rounded p-4 ring-1 ring-black ">Contact Me</button>
+//   </div>
+// </div>
+
+// </div> 
