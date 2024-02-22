078385fe2ce2105bb9a9423005a00fe080c1a87b
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-23 02:51:14 +0600

    up

--- components/TransitionProvider.jsx
+++ components/TransitionProvider.jsx
@@ -7,7 +7,11 @@ const TransitionProvider = ({children}) => {
     return (
         <AnimatePresence>
             <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
-                <motion.div/>
+                <motion.div className='rounded-b-[100px] z-40'
+                animate={{height:"0vh"}}
+                exit={{height:"100vh"}}
+                transition={{duration:0.5, ease: "easeInOut"}}
+                />
                 <div className="h-24"><NavBar />
                 </div >
                {children}
