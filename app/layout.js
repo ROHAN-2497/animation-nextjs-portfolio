92075d2d7096d9e868723f6d7f0f58d569d6fc1b
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-22 18:21:15 +0600

    up

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import TransitionProvider from "@/components/TransitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,12 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}><div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
-        <div className="h-24"><NavBar/>
-        
-        </div>
-        {children}
-        </div></body>
+      <body className={inter.className}><TransitionProvider>{children}</TransitionProvider></body>
     </html>
   );
 }
--- components/TransitionProvider.jsx
+++ components/TransitionProvider.jsx
@@ -1,14 +1,16 @@
-import { AnimatePresence } from 'framer-motion';
+"use client"
+import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import NavBar from './NavBar';
 
-const TransitionProvider = () => {
+const TransitionProvider = ({children}) => {
     return (
         <AnimatePresence>
             <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
+                <motion.div/>
                 <div className="h-24"><NavBar />
-                </div>
-                {children}
+                </div >
+               {children}
             </div>
         </AnimatePresence>
     );
