86a0c78dec17e655688b38ec8dcb4dd4f0f8392a
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-23 11:38:11 +0600

    up

--- components/TransitionProvider.jsx
+++ components/TransitionProvider.jsx
@@ -2,14 +2,16 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import NavBar from './NavBar';
+import { usePathname } from 'next/navigation';
 
 const TransitionProvider = ({children}) => {
+    const pathName= usePathname()
     return (
         <AnimatePresence>
-            <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
+            <div key={pathName} className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
                 <motion.div className='rounded-b-[100px] z-40'
                 animate={{height:"0vh"}}
-                exit={{height:"100vh"}}
+                exit={{height:"140vh"}}
                 transition={{duration:0.5, ease: "easeInOut"}}
                 />
                 <div className="h-24"><NavBar />
