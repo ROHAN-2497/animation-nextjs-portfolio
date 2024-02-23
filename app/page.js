7e3e7f2c9c7daf5b321850409cb690ff5025425f
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-23 12:39:56 +0600

    up

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
 "use client"
 import Image from "next/image";
+import {  motion } from 'framer-motion';
+
 
 export default function Home() {
   return (
+    <motion.div
+    className="h-full"
+    initial={{y: "-200vh"}}
+    animate={{y:"0%"}}
+    transition={{duration:1}}
+    >
+
     <div className=" h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* IMAGE CONTAINER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
@@ -23,6 +32,7 @@ export default function Home() {
         </div>
       </div>
     </div>
+    </motion.div>
   );
 }
 
--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,10 +1,16 @@
+"use client"
 import React from 'react';
-
+import { motion } from 'framer-motion';
 const Portfolio = () => {
     return (
-        <div>
+        <motion.div
+            className="h-full"
+            initial={{ y: "-200vh" }}
+            animate={{ y: "0%" }}
+            transition={{ duration: 1 }}
+        >
             Portfolio
-        </div>
+        </motion.div>
     );
 };
 
