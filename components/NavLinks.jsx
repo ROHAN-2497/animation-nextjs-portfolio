d35c45b813975e58a713a9d000ee421df7c69462
ROHAN-2497 <rohanislam3669@gmail.com>
2024-03-03 00:10:56 +0600

    emailJS

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -1,15 +1,20 @@
-"use client"
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
-const NavLinks = ({link}) => {
-const pathName = usePathname()
-    return (
-        <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white"}`} href={link.title}>
-            {link.title}
-        </Link>
-    );
+const NavLinks = ({ link }) => {
+  const pathName = usePathname();
+  return (
+    <Link
+      className={`rounded p-1 ${
+        pathName === link.url && "bg-black text-white"
+      }`}
+      href={link.url}
+    >
+      {link.title}
+    </Link>
+  );
 };
 
 export default NavLinks;
