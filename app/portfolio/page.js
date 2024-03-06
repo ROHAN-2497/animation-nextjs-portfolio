7c32eede84df6e0b05ffee2fe7763135e3b8dc6e
ROHAN-2497 <rohanislam3669@gmail.com>
2024-03-06 13:27:43 +0600

    up

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -8,10 +8,10 @@ const items = [
   {
     id: 1,
     color: "from-red-300 to-blue-300",
-    title: "React Commerce",
+    title: "Burger Shop",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
+    img: "/718TEkaopJL.jpg",
+    link: "https://burger-shop-1f68d.web.app/",
   },
   {
     id: 2,
